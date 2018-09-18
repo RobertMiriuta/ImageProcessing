c205f2562983aada136db090a20f72ccc9a90052
SquaredCanine <SquaredCanine@users.noreply.github.com>
2018-09-18 14:04:19 +0200

    LETSGOO

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace INFOIBV
+{
+    partial class INFOIBV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.openImageDialog = new System.Windows.Forms.OpenFileDialog();
+            this.imageFileName = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.saveImageDialog = new System.Windows.Forms.SaveFileDialog();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Location = new System.Drawing.Point(16, 15);
+            this.LoadImageButton.Margin = new System.Windows.Forms.Padding(4);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(131, 28);
+            this.LoadImageButton.TabIndex = 0;
+            this.LoadImageButton.Text = "Load image...";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // openImageDialog
+            // 
+            this.openImageDialog.Filter = "Bitmap files (*.bmp;*.gif;*.jpg;*.png;*.tiff;*.jpeg)|*.bmp;*.gif;*.jpg;*.png;*.ti" +
+    "ff;*.jpeg";
+            this.openImageDialog.InitialDirectory = "..\\..\\images";
+            // 
+            // imageFileName
+            // 
+            this.imageFileName.Location = new System.Drawing.Point(155, 17);
+            this.imageFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.imageFileName.Name = "imageFileName";
+            this.imageFileName.ReadOnly = true;
+            this.imageFileName.Size = new System.Drawing.Size(420, 22);
+            this.imageFileName.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 55);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(683, 630);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // applyButton
+            // 
+            this.applyButton.Location = new System.Drawing.Point(856, 15);
+            this.applyButton.Margin = new System.Windows.Forms.Padding(4);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(137, 28);
+            this.applyButton.TabIndex = 3;
+            this.applyButton.Text = "Apply";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+            // 
+            // saveImageDialog
+            // 
+            this.saveImageDialog.Filter = "Bitmap file (*.bmp)|*.bmp";
+            this.saveImageDialog.InitialDirectory = "..\\..\\images";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(1264, 14);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(127, 28);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save as BMP...";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(708, 55);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(683, 630);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(1001, 17);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(255, 25);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 6;
+            this.progressBar.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "negative",
+            "grayscale",
+            "contrastadjustment",
+            "gaussian"});
+            this.comboBox1.Location = new System.Drawing.Point(688, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(161, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(582, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(582, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(582, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Visible = false;
+            // 
+            // INFOIBV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1403, 709);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.applyButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.imageFileName);
+            this.Controls.Add(this.LoadImageButton);
+            this.Location = new System.Drawing.Point(10, 10);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "INFOIBV";
+            this.ShowIcon = false;
+            this.Text = "INFOIBV";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.OpenFileDialog openImageDialog;
+        private System.Windows.Forms.TextBox imageFileName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button applyButton;
+        private System.Windows.Forms.SaveFileDialog saveImageDialog;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace INFOIBV
+{
+    public partial class INFOIBV : Form
+    {
+        private Bitmap InputImage;
+        private Bitmap OutputImage;
+
+        public INFOIBV()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+           if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
+            {
+                string file = openImageDialog.FileName;                     // Get the file name
+                imageFileName.Text = file;                                  // Show file name
+                if (InputImage != null) InputImage.Dispose();               // Reset image
+                InputImage = new Bitmap(file);                              // Create new Bitmap from file
+                if (InputImage.Size.Height <= 0 || InputImage.Size.Width <= 0 ||
+                    InputImage.Size.Height > 512 || InputImage.Size.Width > 512) // Dimension check
+                    MessageBox.Show("Error in image dimensions (have to be > 0 and <= 512)");
+                else
+                    pictureBox1.Image = (Image) InputImage;                 // Display input image
+            }
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            if (InputImage == null) return;                                 // Get out if no input image
+            if (OutputImage != null) OutputImage.Dispose();                 // Reset output image
+            OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // Create new output image
+            Color[,] Image = new Color[InputImage.Size.Width, InputImage.Size.Height]; // Create array to speed-up operations (Bitmap functions are very slow)
+
+            // Setup progress bar
+            progressBar.Visible = true;
+            progressBar.Minimum = 1;
+            progressBar.Maximum = InputImage.Size.Width * InputImage.Size.Height;
+            progressBar.Value = 1;
+            progressBar.Step = 1;
+
+            // Copy input Bitmap to array            
+            for (int x = 0; x < InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height; y++)
+                {
+                    Image[x, y] = InputImage.GetPixel(x, y);                // Set pixel color in array at (x,y)
+                }
+            }
+
+            //==========================================================================================
+            // TODO: include here your own code
+            // example: create a negative image
+            switch (comboBox1.Text)
+            {
+                case "grayscale":
+                    Image = conversionGrayscale(Image);
+                    break;
+                case "negative":
+                    Image = conversionNegative(Image);
+                    break;
+                case "contrastadjustment":
+                    double percentage = Convert.ToDouble(textBox1.Text);
+                    if (percentage >= 0.50)
+                    {
+                        MessageBox.Show("Percentage selected is too high");
+                        return;
+                    }
+                    else
+                    {
+                        Image = conversionContrastAdjustment(Image, Convert.ToDouble(textBox1.Text));
+                    }
+                    break;
+                case "gaussian":
+                    if (textBox2.Text.Equals("") || textBox3.Equals("")) return;
+                    Image = conversionGaussian(Image, Convert.ToDouble(textBox2.Text), Convert.ToInt16(textBox3.Text));
+                    break;
+                default:
+                    Console.WriteLine("Nothing matched");
+                    break;
+            }
+           
+
+            // Copy array to output Bitmap
+            for (int x = 0; x < Image.GetLength(0); x++)
+            {
+                for (int y = 0; y < Image.GetLength(1); y++)
+                {
+                    OutputImage.SetPixel(x, y, Image[x, y]);               // Set the pixel color at coordinate (x,y)
+                }
+            }
+            
+            pictureBox2.Image = (Image)OutputImage;                         // Display output image
+            progressBar.Visible = false;                                    // Hide progress bar
+        }
+
+        private Color[,] conversionGrayscale(Color[,] image)
+        {
+            for (int x = 0; x < InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height; y++)
+                {
+                    Color pixelColor = image[x, y];                         // Get the pixel color at coordinate (x,y)
+                    int convertedRedColor = (int)(pixelColor.R * 0.299);
+                    int convertedGreenColor = (int)(pixelColor.G * 0.587);
+                    int convertedBlueColor = (int)(pixelColor.B * 0.114);
+                    int Y = convertedRedColor + convertedGreenColor + convertedBlueColor;
+                    if (Y < 0)
+                    {
+                        Y = 0;
+                    }
+
+                    if (Y > 255)
+                    {
+                        Y = 255;
+                    }
+
+                    Color updatedColor = Color.FromArgb(Y, Y, Y);
+                    image[x, y] = updatedColor;                             // Set the new pixel color at coordinate (x,y)
+                    progressBar.PerformStep();                              // Increment progress bar
+                }
+
+            }
+            return image;
+        }
+
+        private Color[,] conversionNegative(Color[,] image)
+        {
+            for (int x = 0; x < InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height; y++)
+                {
+                    Color pixelColor = image[x, y];                         // Get the pixel color at coordinate (x,y)
+                    Color updatedColor = Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B); // Negative image
+                    image[x, y] = updatedColor;                             // Set the new pixel color at coordinate (x,y)
+                    progressBar.PerformStep();                              // Increment progress bar
+                }
+
+            }
+            return image;
+        }
+
+        private Color[,] conversionContrastAdjustment(Color[,] image, double percentage)
+        {
+            int low_R;
+            int low_G;
+            int low_B;
+            int high_R;
+            int high_G;
+            int high_B;
+
+            int[] histogram_red = new int[256];
+            int[] histogram_green = new int[256];
+            int[] histogram_blue = new int[256];
+            int amount_of_pixels = InputImage.Size.Width * InputImage.Size.Height;
+            for (int x = 0; x < InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height; y++)
+                {
+                    Color pixelColor = image[x, y];                         // Get the pixel color at coordinate (x,y)
+                    histogram_red[pixelColor.R]++;
+                    histogram_green[pixelColor.G]++;
+                    histogram_blue[pixelColor.B]++;
+                }
+
+            }
+
+            int percentile = (int) (percentage * amount_of_pixels);
+            Console.Out.WriteLine("the percentile is set at: " + percentile);
+
+            low_R = getColorAtPercentileLowFromHistogram(histogram_red, percentile);
+            high_R = getColorAtPercentileHighFromHistogram(histogram_red, percentile);
+
+            low_G = getColorAtPercentileLowFromHistogram(histogram_blue, percentile);
+            high_G = getColorAtPercentileHighFromHistogram(histogram_blue, percentile);
+
+            low_B = getColorAtPercentileLowFromHistogram(histogram_blue, percentile);
+            high_B = getColorAtPercentileHighFromHistogram(histogram_blue, percentile);
+
+            double kR = 255 / (high_R - low_R);
+            double kG = 255 / (high_G - low_G);
+            double kB = 255 / (high_B - low_B);
+
+
+            for (int x = 0; x < InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height; y++)
+                {
+                    Color pixelColor = image[x, y];
+                    int updatedRed = (int) (kR * (pixelColor.R - low_R));
+                    int updatedGreen = (int) (kG * (pixelColor.G - low_G));
+                    int updatedBlue = (int) (kG * (pixelColor.B - low_B));
+                    if (updatedRed > 255) updatedRed = 255;
+                    if (updatedGreen > 255) updatedGreen = 255;
+                    if (updatedBlue > 255) updatedBlue = 255;
+                    if (updatedRed < 0) updatedRed = 0;
+                    if (updatedGreen < 0) updatedGreen = 0;
+                    if (updatedBlue < 0) updatedBlue = 0;
+                    Color updatedColor = Color.FromArgb(updatedRed, updatedGreen, updatedBlue);
+                    image[x, y] = updatedColor;                             // Set the new pixel color at coordinate (x,y)
+                    progressBar.PerformStep();                              // Increment progress bar
+                }
+            }
+            return image;
+        }
+
+        private Color[,] conversionGaussian(Color[,] image, double sigma, int size)
+        {
+            double[,] gaussianFilter = createGaussianFilter(sigma, size);
+            return applyFilterToImage(image, gaussianFilter);
+        }
+
+        private double[,] createGaussianFilter(double sigma, int size)
+        {
+            double[,] gaussianFilter = new double[size, size];
+            double r;
+            double s = 2.0 * sigma * sigma;
+
+            //This is used later for normalization
+            double sum = 0.0;
+            int halfSize = (size - 1) / 2;
+            for (int x = -halfSize; x <= halfSize; x++)
+            {
+                for (int y = -halfSize; y <= halfSize; y++)
+                {
+                    r = Math.Sqrt(x * x + y * y);
+                    int xIndice = x + halfSize;
+                    int yIndice = y + halfSize;
+                    double resultGaussian = (Math.Exp(-(r * r) / s)) / (Math.PI * s);
+                    gaussianFilter[xIndice, yIndice] = resultGaussian;
+                    sum += resultGaussian;
+                }
+            }
+
+            //Normalization
+            for (int i = 0; i < size; ++i)
+            { 
+                for (int j = 0; j < size; ++j)
+                {
+                    gaussianFilter[i, j] /= sum;
+                }
+            }
+            return gaussianFilter;
+        }
+
+        private Color[,] applyFilterToImage(Color[,] image, double[,] filter)
+        {
+            int halfSize = (filter.GetLength(0) - 1) / 2;
+            int xBorder = (filter.GetLength(0) - 1) / 2;
+            int yBorder = (filter.GetLength(1) - 1) / 2;
+            Color[,] newImage = new Color[InputImage.Size.Width, InputImage.Size.Height];
+            for (int x = xBorder; x < (InputImage.Size.Width - xBorder); x++)
+            {
+                for (int y = yBorder; y < (InputImage.Size.Height - yBorder); y++)
+                {
+                    double updatedRed = 0.0;
+                    double updatedGreen = 0.0;
+                    double updatedBlue = 0.0;
+                    for (int xFilter = -halfSize; xFilter <= halfSize; xFilter++)
+                    {
+                        for (int yFilter = -halfSize; yFilter <= halfSize; yFilter++)
+                        {
+                            Color filterColor = image[x - xFilter, y - yFilter];
+                            updatedRed += filter[(xFilter + halfSize), (yFilter + halfSize)] * filterColor.R;
+                            updatedGreen += filter[(xFilter + halfSize), (yFilter + halfSize)] * filterColor.G;
+                            updatedBlue += filter[(xFilter + halfSize), (yFilter + halfSize)] * filterColor.B;
+                        }
+                    }
+                    Color updatedColor = Color.FromArgb(Convert.ToInt32(updatedRed), Convert.ToInt32(updatedGreen), Convert.ToInt32(updatedBlue));
+                    newImage[x-xBorder, y-yBorder] = updatedColor;                             // Set the new pixel color at coordinate (x,y)
+                    progressBar.PerformStep();                              // Increment progress bar
+                }
+
+            }
+            return newImage;
+        }
+        private int getColorAtPercentileLowFromHistogram(int[] histogram_array, int percentile)
+        {
+            for (int i = 0; i < 255; i++)
+            {
+                percentile = percentile - histogram_array[i];
+                if (percentile < 0)
+                {
+                    return i;
+                }
+            }
+
+            return 255;
+        }
+
+        private int getColorAtPercentileHighFromHistogram(int[] histogram_array, int percentile)
+        {
+            for (int i = 255; 0 < i; i--)
+            {
+                percentile = percentile - histogram_array[i];
+                if (percentile < 0)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (OutputImage == null) return;                                // Get out if no output image
+            if (saveImageDialog.ShowDialog() == DialogResult.OK)
+                OutputImage.Save(saveImageDialog.FileName);                 // Save the output image
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Equals("gaussian"))
+            {
+                textBox1.Visible = false;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+            }
+            else
+            {
+                textBox1.Visible = true;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+            }
+        }
+    }
+}
